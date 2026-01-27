44f2bebc19068c56c934502253a5e080dfaa7df8
Roshani Sahu <roshani032003@gmail.com>
2026-01-27 17:36:07 +0530

    heading colour

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -123,8 +123,8 @@ const Dashboard = () => {
       <section className="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-6 mb-4 md:mb-6">
         {/* Stock Summary */}
         <div className="lg:col-span-2 bg-white border border-neutral-200 rounded-lg">
-          <div className="p-3 md:p-4 border-b flex justify-between items-center">
-            <h3 className="text-sm md:text-base text-neutral-900">
+          <div className="p-3 md:p-4 bg-[#F8FAFC] border-b flex justify-between items-center">
+            <h3 className="text-sm  md:text-base text-neutral-900">
               Stock Summary
             </h3>
             <button className="text-xs text-neutral-600 hover:underline">
