54b758f10fe7cf5b6f96da4dacfbd478ef15cfe8
Roshani Sahu <roshani032003@gmail.com>
2026-02-13 11:50:24 +0530

    backend delete folder

--- backend/src/utils/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ApiError } from "./ApiError.js";
-export { default as ApiResponse } from "./ApiResponse.js";
-export { default as asyncHandler } from "./asyncHandler.js";
-export { default as Pagination } from "./pagination.js";
