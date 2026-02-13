867dd80ec7d82f5df31c108f1281a51c69909c3f
shrivanshu04 <shrivanshu4@gmail.com>
2026-02-13 11:03:26 +0530

    111

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -93,20 +93,7 @@ const Sidebar = ({ onClose }) => {
 
           {/* 2. Masters */}
           <SidebarSection title="Masters" defaultOpen={true}>
-            <NavLink
-              to="/masters/firm-master"
-              onClick={onClose}
-              className={({ isActive }) =>
-                `${linkBase} ${
-                  isActive
-                    ? "bg-neutral-100 text-neutral-900"
-                    : "text-[#CBD5E1] hover:bg-neutral-100 hover:text-neutral-900"
-                }`
-              }
-            >
-              <FaBuilding className="w-4 h-4" />
-              Firm Master
-            </NavLink>
+       
             
              <NavLink
               to="/masters/party-master"
