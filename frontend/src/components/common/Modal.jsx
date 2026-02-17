8d37bc4d2ab7242f34cb4fd00e53ce60391da9c1
Roshani Sahu <roshani032003@gmail.com>
2026-02-17 16:11:06 +0530

    1111

--- frontend/src/components/common/Modal.jsx
+++ frontend/src/components/common/Modal.jsx
@@ -22,7 +22,7 @@ const Modal = ({
   };
 
   return (
-    <div className="fixed inset-0 z-50 overflow-y-auto">
+    <div className="fixed -inset-5 z-50 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
         {/* Backdrop */}
         <div 
