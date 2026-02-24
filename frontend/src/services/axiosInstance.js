dc7bdc3b6e0f189228bc6a557645ca3614d4563d
Roshani Sahu <roshani032003@gmail.com>
2026-02-24 12:30:14 +0530

    1

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api/v1";
+const API_BASE_URL = "http://13.233.250.55:3000/api/v1";
 let isHandlingUnauthorized = false;
 const AUTH_OPTIONAL_PATHS = ["/auth/login", "/auth/admin/register", "/health"];
 
