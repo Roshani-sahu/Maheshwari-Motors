9c9291bc59a291a392ee1331b595bb8625a498d4
tushar-gour <tushargour004@gmail.com>
2026-02-24 13:50:51 +0530

    update :: 2026-02-24 13:50:50

--- backend/src/services/master/bank.service.js
+++
@@ -1,6 +0,0 @@
-import { asyncHandler } from "../../utils";
-
-class BankService {
-  getTransactions = asyncHandler(async (req, res) => {});
-  createTransaction = asyncHandler(async (req, res) => {});
-}
